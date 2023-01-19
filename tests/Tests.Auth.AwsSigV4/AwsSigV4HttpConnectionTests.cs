20c0adc143bed25ba45798e3d7cb0f75438e7f1d
Thomas Farr <tsfarr@amazon.com>
2023-01-19 16:54:56 +1300

    Disable meta header in test to make headers & signature stable across platforms
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- tests/Tests.Auth.AwsSigV4/AwsSigV4HttpConnectionTests.cs
+++ tests/Tests.Auth.AwsSigV4/AwsSigV4HttpConnectionTests.cs
@@ -27,9 +27,9 @@ public class AwsSigV4HttpConnectionTests
 	private static readonly DateTime TestSigningTime = new(2023, 01, 13, 16, 08, 37, DateTimeKind.Utc);
 
 	[TU]
-	[InlineData("es", "275d72b784a3183861ae13b4fcb486ff31a7a9dd7f2d6ebb780f89008aa689cc")]
-	[InlineData("aoss", "89fb38096ed2e9f4a2908d1e40c3e1b7ac68fc4ab2f723feddec61b4ada4607a")]
-	[InlineData("arbitrary", "9bdf7d1cf23d8ca4b41f84cd15aafbc7b665dd4985cf68258c9e7ac40311521b")]
+	[InlineData("es", "10c9be415f4b9f15b12abbb16bd3e3730b2e6c76e0cf40db75d08a44ed04a3a1")]
+	[InlineData("aoss", "34903aef90423aa7dd60575d3d45316c6ef2d57bbe564a152b41bf8f5917abf6")]
+	[InlineData("arbitrary", "156e65c504ea2b2722a481b7515062e7692d27217b477828854e715f507e6a36")]
 	public async Task SignsRequestCorrectly(string service, string expectedSignature)
 	{
 		var response = new HttpResponseMessage(HttpStatusCode.OK);
@@ -58,12 +58,15 @@ public class AwsSigV4HttpConnectionTests
 
 		sentRequest.ShouldHaveHeader("x-amz-date", "20230113T160837Z");
 		sentRequest.ShouldHaveHeader("x-amz-content-sha256", "4c770eaed349122a28302ff73d34437cad600acda5a9dd373efc7da2910f8564");
-		sentRequest.ShouldHaveHeader("Authorization", $"AWS4-HMAC-SHA256 Credential=test-access-key/20230113/ap-southeast-2/{service}/aws4_request, SignedHeaders=accept;content-type;host;opensearch-client-meta;x-amz-content-sha256;x-amz-date, Signature={expectedSignature}");
+		sentRequest.ShouldHaveHeader("Authorization", $"AWS4-HMAC-SHA256 Credential=test-access-key/20230113/ap-southeast-2/{service}/aws4_request, SignedHeaders=accept;content-type;host;x-amz-content-sha256;x-amz-date, Signature={expectedSignature}");
 	}
 
-	private static OpenSearchClient CreateClient(MockHttpMessageHandler.Handler handler, string uri, string service) =>
-		new(new ConnectionSettings(
-			new Uri(uri),
-			new TestableAwsSigV4HttpConnection(TestCredentials, TestRegion, service, new FixedDateTimeProvider(TestSigningTime), handler)
-			));
+	private static OpenSearchClient CreateClient(MockHttpMessageHandler.Handler handler, string uri, string service)
+	{
+		var connection =
+			new TestableAwsSigV4HttpConnection(TestCredentials, TestRegion, service, new FixedDateTimeProvider(TestSigningTime), handler);
+		var settings = new ConnectionSettings(new Uri(uri), connection);
+		settings.DisableMetaHeader(); // Make headers & signature stable across platforms for testing
+		return new OpenSearchClient(settings);
+	}
 }
