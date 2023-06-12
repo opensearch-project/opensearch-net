c19a6746e613b82945d9d45056b711acc172e859
Thomas Farr <tsfarr@amazon.com>
2023-06-13 07:26:44 +1200

    Revert "Remove ApiGenerator (#156)" (#222)
    * Revert "Remove ApiGenerator (#156)"
    
    This reverts commit 7db02e979b19c748879b74485f04f6c352a0d967.
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    
    * Update packages.lock.json
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    
    ---------
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

---
+++ src/ApiGenerator/Configuration/Overrides/GlobalOverrides.cs
@@ -0,0 +1,76 @@
+/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+/*
+* Modifications Copyright OpenSearch Contributors. See
+* GitHub history for details.
+*
+*  Licensed to Elasticsearch B.V. under one or more contributor
+*  license agreements. See the NOTICE file distributed with
+*  this work for additional information regarding copyright
+*  ownership. Elasticsearch B.V. licenses this file to you under
+*  the Apache License, Version 2.0 (the "License"); you may
+*  not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+
+using System.Collections.Generic;
+
+namespace ApiGenerator.Configuration.Overrides
+{
+    public class GlobalOverrides : EndpointOverridesBase
+    {
+        public IDictionary<string, Dictionary<string, string>> ObsoleteEnumMembers { get; set; } = new Dictionary<string, Dictionary<string, string>>()
+        {
+            { "VersionType", new Dictionary<string, string>() { { "force", "Force is no longer accepted by the server as of 7.5.0 and will result in an error when used" } } }
+        };
+
+        public override IDictionary<string, string> ObsoleteQueryStringParams { get; set; } = new Dictionary<string, string>
+        {
+            { "copy_settings", "" }
+        };
+
+        public override IDictionary<string, string> RenameQueryStringParams { get; } = new Dictionary<string, string>
+        {
+            { "_source", "source_enabled" },
+            { "_source_includes", "source_includes" },
+            { "_source_excludes", "source_excludes" },
+            { "rest_total_hits_as_int", "total_hits_as_integer" },
+            { "docvalue_fields", "doc_value_fields" },
+            { "q", "query_on_query_string" },
+            //make cat parameters more descriptive
+            { "h", "Headers" },
+            { "s", "sort_by_columns" },
+            { "v", "verbose" },
+            { "ts", "include_timestamp" },
+            { "if_seq_no", "if_sequence_number" },
+            { "seq_no_primary_term", "sequence_number_primary_term" },
+        };
+
+        public override IEnumerable<string> RenderPartial => new[]
+        {
+            "stored_fields",
+            "docvalue_fields"
+        };
+
+        public override IEnumerable<string> SkipQueryStringParams { get; } = new[]
+        {
+            "copy_settings", //this still needs a PR?
+            "source", // allows the body to be specified as a request param, we do not want to advertise this with a strongly typed method
+            "timestamp",
+            "time"
+        };
+    }
+}
---
+++ src/ApiGenerator/Domain/Code/HighLevel/Requests/Constructor.cs
@@ -0,0 +1,135 @@
+/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+/*
+* Modifications Copyright OpenSearch Contributors. See
+* GitHub history for details.
+*
+*  Licensed to Elasticsearch B.V. under one or more contributor
+*  license agreements. See the NOTICE file distributed with
+*  this work for additional information regarding copyright
+*  ownership. Elasticsearch B.V. licenses this file to you under
+*  the Apache License, Version 2.0 (the "License"); you may
+*  not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApiGenerator.Configuration;
+using ApiGenerator.Domain.Specification;
+
+namespace ApiGenerator.Domain.Code.HighLevel.Requests
+{
+    public class Constructor
+    {
+        private static readonly string Indent = $"{Environment.NewLine}\t\t";
+        public string AdditionalCode { get; set; } = string.Empty;
+        public bool Parameterless { get; set; }
+        public string Body { get; set; }
+        public string Description { get; set; }
+        public string Generated { get; set; }
+
+        public static IEnumerable<Constructor> DescriptorConstructors(CsharpNames names, UrlInformation url)
+        {
+            var m = names.DescriptorName;
+            var generic = FirstGeneric(names.GenericsDeclaredOnDescriptor);
+            var generateGeneric = !string.IsNullOrEmpty(generic);
+            return GenerateConstructors(url, true, generateGeneric, m, generic);
+        }
+
+        public static IEnumerable<Constructor> RequestConstructors(CsharpNames names, UrlInformation url, bool inheritsFromPlainRequestBase)
+        {
+            var generic = FirstGeneric(names.GenericsDeclaredOnRequest);
+            var generateGeneric = CodeConfiguration.GenericOnlyInterfaces.Contains(names.RequestInterfaceName) || !inheritsFromPlainRequestBase;
+            return GenerateConstructors(url, inheritsFromPlainRequestBase, generateGeneric, names.RequestName, generic);
+        }
+
+        private static string FirstGeneric(string fullGenericString) =>
+            fullGenericString?.Replace("<", "").Replace(">", "").Split(",").First().Trim();
+
+        private static IEnumerable<Constructor> GenerateConstructors(
+            UrlInformation url,
+            bool inheritsFromPlainRequestBase,
+            bool generateGeneric,
+            string typeName,
+            string generic
+        )
+        {
+            var ctors = new List<Constructor>();
+
+            var paths = url.Paths.ToList();
+
+            if (url.IsPartless) return ctors;
+
+            ctors.AddRange(from path in paths
+                let baseArgs = inheritsFromPlainRequestBase ? path.RequestBaseArguments : path.TypedSubClassBaseArguments
+                let constParams = path.ConstructorArguments
+                let generated = $"public {typeName}({constParams}) : base({baseArgs})"
+                select new Constructor
+                {
+                    Parameterless = string.IsNullOrEmpty(constParams),
+                    Generated = generated,
+                    Description = path.GetXmlDocs(Indent),
+                    //Body = isDocumentApi ? $" => Q(\"routing\", new Routing(() => AutoRouteDocument()));" : string.Empty
+                    Body = string.Empty
+                });
+
+            if (generateGeneric && !string.IsNullOrWhiteSpace(generic))
+            {
+                ctors.AddRange(from path in paths.Where(path => path.HasResolvableArguments)
+                    let baseArgs = path.AutoResolveBaseArguments(generic)
+                    let constructorArgs = path.AutoResolveConstructorArguments
+                    let baseOrThis = inheritsFromPlainRequestBase
+                        ? "this"
+                        : "base"
+                    let generated = $"public {typeName}({constructorArgs}) : {baseOrThis}({baseArgs})"
+                    select new Constructor
+                    {
+                        Parameterless = string.IsNullOrEmpty(constructorArgs),
+                        Generated = generated,
+                        Description = path.GetXmlDocs(Indent, skipResolvable: true),
+                        Body = string.Empty
+                    });
+
+                if (url.TryGetDocumentApiPath(out var docPath))
+                {
+                    var docPathBaseArgs = docPath.DocumentPathBaseArgument(generic);
+                    var docPathConstArgs = docPath.DocumentPathConstructorArgument(generic);
+                    ctors.Add(new Constructor
+                    {
+                        Parameterless = string.IsNullOrEmpty(docPathConstArgs),
+                        Generated = $"public {typeName}({docPathConstArgs}) : this({docPathBaseArgs})",
+
+                        AdditionalCode = $"partial void DocumentFromPath({generic} document);",
+                        Description = docPath.GetXmlDocs(Indent, skipResolvable: true, documentConstructor: true),
+                        Body = "=> DocumentFromPath(documentWithId);"
+                    });
+                }
+            }
+            var constructors = ctors.GroupBy(c => c.Generated.Split(new[] { ':' }, 2)[0]).Select(g => g.Last()).ToList();
+            if (!constructors.Any(c=>c.Parameterless))
+                constructors.Add(new Constructor
+                {
+                    Parameterless = true,
+                    Generated = $"protected {typeName}() : base()",
+                    Description =
+                        $"///<summary>Used for serialization purposes, making sure we have a parameterless constructor</summary>{Indent}[SerializationConstructor]",
+                });
+            return constructors;
+        }
+    }
+}
