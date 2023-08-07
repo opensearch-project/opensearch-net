a8a81a0268fe445f4c4df6aacdb544f66298b8b1
Thomas Farr <tsfarr@amazon.com>
2023-08-08 09:36:49 +1200

    Run rest-api-spec / YAML tests in CI (#303)
    * Run yaml-test
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    
    * Fixes
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    
    * Separate workflow
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    
    ---------
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>
    (cherry picked from commit dcf543fafac01ef578573116373ce73aa3f91972)

--- tests/Tests.YamlRunner/TestsLocator.fs
+++ tests/Tests.YamlRunner/TestsLocator.fs
@@ -28,39 +28,9 @@
 module Tests.YamlRunner.TestsLocator
 
 open System
-open System.Threading
-open FSharp.Data
+open System.IO
+open System.IO.Compression
 open Tests.YamlRunner.AsyncExtensions
-open ShellProgressBar
-open Tests.YamlRunner
-
-
-let ListFolders namedSuite revision directory = async {
-    let url = TestsDownloader.TestGithubRootUrl namedSuite revision
-    let! (_, html) = TestsDownloader.CachedOrDownload namedSuite revision "_root_" "index.html" url 
-    let doc = HtmlDocument.Parse(html)
-    
-    return
-        doc.CssSelect("div.js-details-container a.js-navigation-open")
-        |> List.map (fun a -> a.InnerText())
-        |> List.filter (fun f -> match directory with | Some s -> f.StartsWith(s, StringComparison.OrdinalIgnoreCase) | None -> true)
-        |> List.filter(fun f -> f.Replace(" ", "") <> "..")
-        |> List.filter (fun f -> not <| f.EndsWith(".asciidoc"))
-}
-    
-let ListFolderFiles namedSuite revision folder fileFilter = async { 
-    let url = TestsDownloader.FolderListUrl namedSuite revision folder
-    let! (_, html) =  TestsDownloader.CachedOrDownload namedSuite revision folder "index.html" url 
-    let doc = HtmlDocument.Parse(html)
-    let yamlFiles =
-        let fileUrl file = (file, TestsDownloader.TestRawUrl namedSuite revision folder file)
-        doc.CssSelect("div.js-details-container a.js-navigation-open")
-        |> List.map(fun a -> a.InnerText())
-        |> List.filter(fun f -> f.EndsWith(".yml"))
-        |> List.filter (fun f -> match fileFilter with | Some s -> f.StartsWith(s, StringComparison.OrdinalIgnoreCase) | None -> true)
-        |> List.map fileUrl
-    return yamlFiles
-}
 
 type YamlFileInfo = { File: string; Yaml: string }
 
@@ -68,44 +38,51 @@ let TestLocalFile file =
     let yaml = System.IO.File.ReadAllText file
     { File = file; Yaml = yaml }
 
-let private downloadTestsInFolder (yamlFiles:list<string * string>) folder namedSuite revision (progress: IProgressBar) subBarOptions = async {
-    let mutable seenFiles = 0;
-    use filesProgress = progress.Spawn(yamlFiles.Length, sprintf "Downloading [0/%i] files in %s" yamlFiles.Length folder, subBarOptions)
-    let actions =
-        yamlFiles
-        |> Seq.map (fun (file, url) -> async {
-            let! (localFile, yaml) =  TestsDownloader.CachedOrDownload namedSuite revision folder file url
-            let i = Interlocked.Increment (&seenFiles)
-            let message = sprintf "Downloaded [%i/%i] files in %s" i yamlFiles.Length folder
-            filesProgress.Tick(message)
-            match String.IsNullOrWhiteSpace yaml with
-            | true ->
-                progress.WriteLine(sprintf "Skipped %s since it returned no data" url)
-                return None
-            | _ ->
-                return Some {File = localFile; Yaml = yaml}
-        })
-        |> Seq.toList
-        
-    let! completed = Async.ForEachAsync 4 actions
-    let files = completed |> List.choose id;
-    return files 
-}
-
 type LocateResults = { Folder: string; Paths: YamlFileInfo list } 
 
-let DownloadTestsInFolder folder fileFilter namedSuite revision (progress: IProgressBar) subBarOptions = async {
-    let! token = Async.StartChild <| ListFolderFiles namedSuite revision folder fileFilter
-    let! yamlFiles = token
-    let! localFiles = async {
-       match yamlFiles.Length with
-       | 0 ->
-           progress.WriteLine(sprintf "%s folder yielded no tests (fileFilter: %O)" folder fileFilter)
-           return List.empty
-       | _ ->
-           let! result = downloadTestsInFolder yamlFiles folder namedSuite revision progress subBarOptions
-           return result
+let TemporaryPath revision suite = lazy(Path.Combine(Path.GetTempPath(), "opensearch", $"tests-%s{suite}-%s{revision}"))
+
+let ExtractTestsInFolder folder (entries:seq<string * ZipArchiveEntry>) fileFilter namedSuite revision = async {
+    let parent = (TemporaryPath revision "_").Force()
+    let directory = Path.Combine(parent, folder)
+    
+    let cachedOrExtract file (entry: ZipArchiveEntry) = async {
+        let file = Path.Combine(directory, file)
+        let fileExists = File.Exists file
+        let directoryExists = Directory.Exists directory
+        let! result = async {
+            match (fileExists, directoryExists) with
+            | true, _ ->
+                let! text = Async.AwaitTask <| File.ReadAllTextAsync file
+                return text
+            | _, d ->
+                if (not d) then Directory.CreateDirectory(directory) |> ignore
+                use zipStream = new StreamReader(entry.Open())
+                let! contents = zipStream.ReadToEndAsync() |> Async.AwaitTask
+                do! File.WriteAllTextAsync(file, contents) |> Async.AwaitTask
+                return contents
+        }
+        return (file, result)
     }
-    progress.Tick()
+    
+    let! localFiles = async {
+        let actions =
+            entries
+            |> Seq.filter (fun (file, _) -> match fileFilter with | Some f -> file.StartsWith(f, StringComparison.OrdinalIgnoreCase) | None -> true)
+            |> Seq.map (fun (file, entry) -> async {
+                let! localFile, yaml = cachedOrExtract file entry
+                match String.IsNullOrWhiteSpace yaml with
+                | true ->
+                    return None
+                | _ ->
+                    return Some {File = localFile; Yaml = yaml}
+            })
+            |> Seq.toList
+            
+        let! completed = Async.ForEachAsync 1 actions
+        let files = completed |> List.choose id;
+        return files 
+    }
+    
     return { Folder = folder; Paths = localFiles }
 }
