9880d8008ad6d688c3a1a117076abb523f204549
Thomas Farr <tsfarr@amazon.com>
2023-08-17 14:19:11 +1200

    Clean up old rest specification files (#319)
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- src/ApiGenerator/Domain/Specification/Documentation.cs
+++ src/ApiGenerator/Domain/Specification/Documentation.cs
@@ -26,12 +26,8 @@
 *  under the License.
 */
 
-using System;
-using Newtonsoft.Json;
-
 namespace ApiGenerator.Domain.Specification
 {
-    [JsonConverter(typeof(DocumentationConverter))]
     public class Documentation
     {
         public string Description { get; set; }
@@ -42,46 +38,5 @@ namespace ApiGenerator.Domain.Specification
             get => _url;
             set => _url = value?.Replace("http://", "https://");
         }
-    }
-
-    public class DocumentationConverter : JsonConverter
-    {
-        public override bool CanWrite { get; } = false;
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotSupportedException();
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-
-            var documentation = new Documentation();
-
-            if (reader.TokenType == JsonToken.String)
-            {
-                documentation.Url = (string)reader.Value;
-                return documentation;
-            }
-
-            while (reader.Read())
-            {
-                if (reader.TokenType == JsonToken.EndObject)
-                    break;
-
-                var prop = (string)reader.Value;
-                switch (prop)
-                {
-                    case "url":
-                        documentation.Url = reader.ReadAsString();
-                        break;
-                    case "description":
-                        documentation.Description = reader.ReadAsString();
-                        break;
-                    default:
-                        throw new Exception($"Property '{prop}' unexpected in documentation object");
-                }
-            }
-            return documentation;
-        }
-
-        public override bool CanConvert(Type objectType) => true;
     }
 }
