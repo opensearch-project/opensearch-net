4bf1c017662a781e5c0ac5c7782a0e118b5ed234
Thomas Farr <tsfarr@amazon.com>
2023-10-27 16:29:09 +1300

    Add tests for DeleteComponentTemplate
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

---
+++ tests/Tests/Cluster/ComponentTemplate/DeleteComponentTemplateApiTests.cs
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+
+using System;
+using OpenSearch.Client.Specification.ClusterApi;
+using OpenSearch.Net;
+using Tests.Core.ManagedOpenSearch.Clusters;
+using Tests.Framework.EndpointTests;
+using Tests.Framework.EndpointTests.TestState;
+
+namespace Tests.Cluster.ComponentTemplate
+{
+	public class DeleteComponentTemplateApiTests
+		: ApiTestBase<WritableCluster, DeleteComponentTemplateResponse, IDeleteComponentTemplateRequest, DeleteComponentTemplateDescriptor,
+			DeleteComponentTemplateRequest>
+	{
+		public DeleteComponentTemplateApiTests(WritableCluster cluster, EndpointUsage usage) : base(cluster, usage) { }
+
+		protected override Func<DeleteComponentTemplateDescriptor, IDeleteComponentTemplateRequest> Fluent => d => d;
+		protected override HttpMethod HttpMethod => HttpMethod.DELETE;
+		protected override DeleteComponentTemplateRequest Initializer => new DeleteComponentTemplateRequest(CallIsolatedValue);
+		protected override string UrlPath => $"/_component_template/{CallIsolatedValue}";
+
+		protected override LazyResponses ClientUsage() => Calls(
+			(client, f) => client.Cluster.DeleteComponentTemplate(CallIsolatedValue, f),
+			(client, f) => client.Cluster.DeleteComponentTemplateAsync(CallIsolatedValue, f),
+			(client, r) => client.Cluster.DeleteComponentTemplate(r),
+			(client, r) => client.Cluster.DeleteComponentTemplateAsync(r)
+		);
+
+		protected override DeleteComponentTemplateDescriptor NewDescriptor() => new DeleteComponentTemplateDescriptor(CallIsolatedValue);
+	}
+}
---
+++ tests/Tests/Cluster/ComponentTemplate/DeleteComponentTemplateUrlTests.cs
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+
+using System.Threading.Tasks;
+using OpenSearch.Client.Specification.ClusterApi;
+using OpenSearch.OpenSearch.Xunit.XunitPlumbing;
+using Tests.Framework.EndpointTests;
+using static Tests.Framework.EndpointTests.UrlTester;
+
+namespace Tests.Cluster.ComponentTemplate
+{
+	public class DeleteComponentTemplateUrlTests
+	{
+		[U] public async Task Urls()
+		{
+			var name = "temp";
+			await DELETE($"/_component_template/{name}")
+					.Fluent(c => c.Cluster.DeleteComponentTemplate(name))
+					.Request(c => c.Cluster.DeleteComponentTemplate(new DeleteComponentTemplateRequest(name)))
+					.FluentAsync(c => c.Cluster.DeleteComponentTemplateAsync(name))
+					.RequestAsync(c => c.Cluster.DeleteComponentTemplateAsync(new DeleteComponentTemplateRequest(name)))
+				;
+		}
+	}
+}
