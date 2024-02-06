6510ccb8f65049a08b613628d12f4df2cce22d4d
Kostas <ghost0002001@hotmail.com>
2024-02-06 19:00:06 +0200

    On fix_375: Added `.Strict(...)`, `.Verbatim(...)`, `.Name(...)` methods on `QueryContainer` to help modify contained query attributes. (#375)
    Signed-off-by: Kostas <ghost0002001@hotmail.com>

---
+++ src/OpenSearch.Client/QueryDsl/Visitor/VerbatimPropagatingVisitor.cs
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: Apache-2.0
+*
+* The OpenSearch Contributors require contributions made to
+* this file be licensed under the Apache-2.0 license or a
+* compatible open source license.
+*/
+
+namespace OpenSearch.Client
+{
+	public class VerbatimPropagatingVisitor : QueryVisitor
+	{
+		private readonly bool _verbatim;
+
+		public VerbatimPropagatingVisitor(bool verbatim) => _verbatim = verbatim;
+
+		public override void Visit(IQuery query)
+		{
+			query.IsVerbatim = _verbatim;
+			base.Visit(query);
+		}
+	}
+}
